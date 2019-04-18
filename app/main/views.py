6082ec75a3806fdda9d8c090aa49072d8184c7a5
YangLP <yyrylp@sina.com>
2019-04-18 22:46:06 +0800

    地址ip修改

--- app/main/views.py
+++ app/main/views.py
@@ -19,7 +19,11 @@ def masternodes():
 def getServerInfoList():
     IP2LocObj = IP2Location.IP2Location()
     IP2LocObj.open('./app/static/IP2LOCATION-LITE-DB5.BIN')
-    # IPList = ['19.5.10.1', '210.125.84.15', '169.235.24.133', '116.21.94.96']
+    # 19.5.10.1
+    # 210.125.84.15
+    # 169.235.24.133
+    # 116.21.94.96
+    # IPList = ['47.111.168.213', '47.102.41.81', '47.88.63.126', '161.117.84.146', '39.97.166.176', '47.92.85.186', '45.58.54.216']
     IPList = []
     file1 = open('./app/static/ip.txt')
     for line in file1:
